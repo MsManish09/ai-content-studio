bea708c6d1abc93a92418854a015af29a6da8fd6
MsManish09 <msmanish680@gmail.com>
2026-03-10 01:43:40 +0530

    frontend initaited. react tailwindcss installed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' m-3 bg-blue-400 text-xl text-red-600 p-2 font-bold ' >app.jsx</h1>
+    </>
+  )
+}
+
+export default App
