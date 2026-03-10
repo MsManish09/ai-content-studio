4488796cde719061656e695b19b0c2c33838f34c
MsManish09 <msmanish680@gmail.com>
2026-03-10 14:31:24 +0530

    cors middleware added. now frontend can communicate iwht backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import authRoutes from './routes/authRoutes.js'
 import GenerationRoutes from './routes/generation.routes.js'
+import cors from 'cors'
 
 export const app = new express()
 
 // middleware to parse JSON
 app.use(express.json())
+
+// cors middleware
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 
 // routes
 // app.use('/', (req, res)=>{
