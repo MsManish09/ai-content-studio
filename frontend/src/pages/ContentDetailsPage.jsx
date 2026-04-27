865159e7fb00085e2f08b26c4f04c81e2652fa73
MsManish09 <msmanish680@gmail.com>
2026-04-27 16:13:30 +0530

    minor go back update in content details component

--- frontend/src/pages/ContentDetailsPage.jsx
+++ frontend/src/pages/ContentDetailsPage.jsx
@@ -65,7 +65,13 @@ export default function ContentDetailsPage(){
                 {/* back btn */}
                 <div className="flex items-center gap-3 mb-5">
                     <IoArrowBack 
-                        onClick={() => navigate(-1)} 
+                        onClick={() => {
+                            if (window.history.length > 1) {
+                                navigate(-1);
+                            } else {
+                                navigate("/"); // fallback -> home page
+                            }
+                        }} 
                         className="w-5 h-5 text-(--color-text-muted) cursor-pointer hover:text-(--color-primary-light) transition-colors duration-200" 
                     />
                 </div>
